0cae897281d509a38feb1f8a098b20bd9ce1f13d
HelHeim111 <dwp2004@icloud.com>
2023-11-20 15:59:44 +0100

    added inserts to bd

--- iis-project/database/db.sql
+++ iis-project/database/db.sql
@@ -61,3 +61,27 @@ CREATE TABLE 'UserSystems' (
     CONSTRAINT 'fk_user' FOREIGN KEY ('user_id') REFERENCES 'Users'('user_id') ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT 'fk_system'FOREIGN KEY ('system_id') REFERENCES 'Systems'('system_id') ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO Users (username, password, role) VALUES ('john_doe', 'password123', 'admin');
+INSERT INTO Users (username, password, role) VALUES ('alice_smith', 'pass456', 'registered');
+
+INSERT INTO DeviceTypes (type_name, description) VALUES ('Thermometer', 'Measures temperature');
+INSERT INTO DeviceTypes (type_name, description) VALUES ('Humidity Sensor', 'Measures humidity levels');
+
+INSERT INTO Parameters (parameter_name, parameter_values) VALUES ('Temperature', 'Celsius, Fahrenheit');
+INSERT INTO Parameters (parameter_name, parameter_values) VALUES ('Humidity', 'Percentage');
+
+INSERT INTO Systems (system_name, system_description, admin_id) VALUES ('SmartHome', 'Home automation system', 1);
+INSERT INTO Systems (system_name, system_description, admin_id) VALUES ('OfficeMonitor', 'Office environment monitoring', 2);
+
+INSERT INTO KPIs (kpi_name, parameter_id, output) VALUES ('Temperature_OK', 1, 1);
+INSERT INTO KPIs (kpi_name, parameter_id, output) VALUES ('Humidity_OK', 2, 1);
+
+INSERT INTO Devices (device_name, device_type_id, description, user_id) VALUES ('LivingRoomThermometer', 1, 'Measures temperature in the living room', 1);
+INSERT INTO Devices (device_name, device_type_id, description, user_id) VALUES ('OfficeHumiditySensor', 2, 'Monitors humidity in the office', 2);
+
+INSERT INTO DeviceTypesParameters (device_type_id, parameter_id) VALUES (1, 1);
+INSERT INTO DeviceTypesParameters (device_type_id, parameter_id) VALUES (2, 2);
+
+INSERT INTO UserSystems (user_id, system_id) VALUES (1, 1);
+INSERT INTO UserSystems (user_id, system_id) VALUES (2, 2);
