6e1e36e50f9984b9e00bc0a0abab139f2a77fc4a
pinlog33 <pinlog33@gmail.com>
2023-11-23 04:37:02 +0100

    added some functionality (creation and deletion of user, editing is almost working), added some additional buttons here and there

--- iis-project/app/Presenters/AdminPresenter.php
+++ iis-project/app/Presenters/AdminPresenter.php
@@ -3,10 +3,21 @@
 namespace App\Presenters;
 
 use Nette;
+use Nette\Database\Context;
+use Nette\Application\UI\Form;
+use Nette\Security\Passwords;
 
 class AdminPresenter extends Nette\Application\UI\Presenter
 {
-    // Called before rendering the dashboard template
+    private $database;
+    private $passwords;
+
+    public function __construct(Context $database, Passwords $passwords)
+    {
+        $this->database = $database;
+        $this->passwords = $passwords;
+    }
+
     public function actionDashboard()
     {
         // Check if the user is logged in and has the 'admin' role
@@ -19,13 +30,130 @@ class AdminPresenter extends Nette\Application\UI\Presenter
                 $this->redirect('Signin:default');
             }
         }
-
-        // Additional logic for the dashboard can be placed here
     }
 
     // Renders the dashboard template
     public function renderDashboard()
     {
-        // Pass data to the template, if needed
+        $this->template->users = $this->database->table('Users')->fetchAll();
     }
+
+    public function renderEditUserForm()
+    {
+        $this->template->form = $this['editUserForm'];
+    }
+
+    // Action to delete a user
+    public function actionDelete($userId)
+    {
+        $user = $this->database->table('Users')->get($userId);
+        if ($user) {
+            $user->delete();
+            $this->flashMessage('Uživatel byl úspěšně smazán.', 'success');
+        } else {
+            $this->flashMessage('Uživatel nenalezen.', 'error');
+        }
+        $this->redirect('Admin:dashboard');
+    }
+
+    // Action to display edit form
+    public function actionEditUserForm($userId)
+    {
+        $user = $this->database->table('Users')->get($userId);
+        if (!$user) {
+            $this->flashMessage('Uživatel nenalezen.', 'error');
+            $this->redirect('Admin:dashboard');
+        }
+
+        $form = $this['editUserForm'];
+        $form->setDefaults($user->toArray());
+    }    
+
+    // Create user form component
+    protected function createComponentCreateUserForm(): Form
+    {
+        $form = new Form;
+        $form->addText('username', 'Uživatelské jméno:')
+                ->setRequired('Prosím zadejte uživatelské jméno.');
+
+        $form->addPassword('password', 'Heslo:')
+                ->setRequired('Prosím zadejte heslo.');
+
+        $form->addSelect('role', 'Role:', [
+            'user' => 'Uživatel',
+            'admin' => 'Administrátor'
+        ])->setRequired('Prosím vyberte roli.');
+
+        $form->addSubmit('create', 'Vytvořit uživatele');
+        $form->onSuccess[] = [$this, 'createUserFormSucceeded'];
+
+        return $form;
+    }
+
+    // Handle create user form submission
+    public function createUserFormSucceeded(Form $form, \stdClass $values): void
+    {
+        // Check if the username already exists
+        $existingUser = $this->database->table('Users')->where('username', $values->username)->fetch();
+        if ($existingUser) {
+            $form->addError('Uživatelské jméno již existuje.');
+            return;
+        }
+        
+        $this->database->table('Users')->insert([
+            'username' => $values->username,
+            'password' => $this->passwords->hash($values->password),
+            'role' => $values->role,
+        ]);
+
+        $this->flashMessage('Uživatel byl úspěšně vytvořen.', 'success');
+        $this->redirect('Admin:dashboard');
+    }
+
+    // Edit user form component
+    protected function createComponentEditUserForm(): Form
+    {
+        $form = new Form;
+        
+        $userId = $this->getParameter('userId');
+        $user = $this->database->table('Users')->get($userId);
+
+        $form->addText('username', 'Uživatelské jméno:')
+                ->setDefaultValue($user->username)
+                ->setRequired('Prosím zadejte uživatelské jméno.');
+
+        $form->addPassword('password', 'Heslo:')
+                ->setRequired(false);
+
+        $form->addSelect('role', 'Role:', [
+            'user' => 'Uživatel',
+            'admin' => 'Administrátor'
+        ])->setDefaultValue($user->role)
+            ->setRequired('Prosím vyberte roli.');
+
+        $form->addSubmit('save', 'Uložit změny');
+        $form->onSuccess[] = [$this, 'editUserFormSucceeded'];
+
+        return $form;
+    }
+
+    // Handle edit user form submission
+    public function editUserFormSucceeded(Form $form, \stdClass $values): void
+    {
+        $userId = $this->getParameter('userId');
+        $user = $this->database->table('Users')->get($userId);
+
+        $dataToUpdate = [
+            'username' => $values->username,
+            'role' => $values->role,
+        ];
+
+        if (!empty($values->password)) {
+            $dataToUpdate['password'] = Passwords::hash($values->password);
+        }
+
+        $user->update($dataToUpdate);
+        $this->flashMessage('Uživatelské údaje byly aktualizovány.', 'success');
+        $this->redirect('Admin:dashboard');
+    } 
 }
